e0dc2fd84fd07d50d3715147a10cd745ceaa78eb
Francis <franciskarani15@gmail.com>
2025-09-23 22:33:58 +0300

    initial migration

--- server/app.py
+++ server/app.py
@@ -1,22 +1,24 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from models import db, Matatu, Matatu_route, Route, Sacco
-from flask_migrate import Migrate 
+from flask_migrate import Migrate
 
 
-app=Flask(__name__)
-api=Api(app)
+app = Flask(__name__)
+api = Api(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///matatu.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///matatu.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
-migrate=Migrate(app, db)
+migrate = Migrate(app, db)
+
 
 class Welcome(Resource):
-     def get(self):
-          return "Hello"
-api.add_resource(Welcome,"/")
+    def get(self):
+        return "Hello"
+
+
+api.add_resource(Welcome, "/")
 
 if __name__ == "__main__":
-     app.run(debug=True)
-     
+    app.run(debug=True, port=5001)
