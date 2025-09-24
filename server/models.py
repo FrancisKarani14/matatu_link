a3d6575febfb49d13c77434f5837a6fe7673ef26
Francis <franciskarani15@gmail.com>
2025-09-24 10:32:08 +0300

    serialize rules

--- server/models.py
+++ server/models.py
@@ -1,9 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.orm import relationship
-from sqlalchemy import ForeignKey
+# from sqlalchemy import ForeignKey
 
-db = SQLAlchemy()
+db = SQLAlchemy() 
 
 
 class Sacco(db.Model, SerializerMixin):
