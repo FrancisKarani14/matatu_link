3ca722b7524cfdbf6c57d88e9e65dc106d44c151
Francis <franciskarani15@gmail.com>
2025-09-25 12:49:20 +0300

    pastes conflcts

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,23 +1,30 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navigation from "./components/Navigation";
-import Home from "./pages/Home";
-import Saccos from "./pages/Saccos";
-import Matatus from "./pages/Matatus";
-import RoutesPage from "./pages/RoutesPage";
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Saccos from './pages/Saccos'
+import Matatus from './pages/Matatus'
+
+import Routes_pages from './pages/Routes_pages'
+
 
 function App() {
   return (
     <Router>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/saccos" element={<Saccos />} />
-        <Route path="/matatus" element={<Matatus />} />
-        <Route path="/routes" element={<RoutesPage />} />
-      </Routes>
+      <div className="min-h-screen bg-gradient-to-br from-gray-50 to-blue-50">
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+
+            <Route path="/saccos" element={<Saccos />} />
+            <Route path="/matatus" element={<Matatus />} />
+            <Route path="/routes" element={<Routes_pages />} />
+          </Routes>
+        </main>
+      </div>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
