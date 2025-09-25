db05778f114d7671d9a7dcb10538a4ef7e18136f
qalicha-dev28 <qalichanajma44@gmail.com>
2025-09-25 11:42:28 +0300

    Install react vite,react-router-dom,tailwind

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Home from '../pages/Home'
+import Saccos from '../pages/Saccos'
+import Matatus from '../pages/Matatus'
+import Routes_pages from '../pages/Routes_pages'
+export default function Navbar() {
+  return (
+    <div>Navbar</div>
+  )
+}
