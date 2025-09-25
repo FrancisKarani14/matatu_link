5c3f979e9f744e6aa32947a26816e6a52895d35f
qalicha-dev28 <qalichanajma44@gmail.com>
2025-09-25 11:44:04 +0300

    Create components/navabar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import Home from '../pages/Home'
 import Saccos from '../pages/Saccos'
 import Matatus from '../pages/Matatus'
 import Routes_pages from '../pages/Routes_pages'
+
 export default function Navbar() {
   return (
     <div>Navbar</div>
