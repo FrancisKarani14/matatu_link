a03f2637caf4e4de19a25e07314078d8a137188e
Francis <franciskarani15@gmail.com>
2025-09-30 00:02:45 +0300

    prepares project for deployment

--- server/app.py
+++ server/app.py
@@ -25,7 +25,7 @@ api = Api(app)
 CORS(app)
 
 
-app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///matatu.db"
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
